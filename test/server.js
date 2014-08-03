197443d76dea66d326a56a68e0a361a2d3d5d0e4
stanley zheng <dev.stanzheng@gmail.com>
2014-08-03 22:54:11 +0100

    renamed to adhere to guidelines

--- test/server.js
+++ test/server.js
@@ -5,13 +5,9 @@ var scorpion = require("../server");
 
 
 describe('Server', function(){
-    beforeEach(function(){
-      console.log('Test Requests');
-    });
     it('Test Root Path', function(done){
       this.timeout(10000);
       request('http://localhost:3000', function (err, res, html){
-        console.log(res.statusCode);
         if(res.statusCode === 200){
           console.log(res.body);
           assert.equal(res.body,"Hello, world!");
