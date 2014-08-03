527c3cf4a45758404f0025056e6d6b134cad3eca
stanley zheng <dev.stanzheng@gmail.com>
2014-08-03 22:46:34 +0100

    wrote test to see request

--- spec/server.js
+++ spec/server.js
@@ -1,11 +1,22 @@
 var assert = require("assert");
 var request = require("request");
+var scorpion = require("../server");
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+
+
+describe('Server', function(){
+    beforeEach(function(){
+      console.log('Test Requests');
+    });
+    it('Test Root Path', function(done){
+      this.timeout(10000);
+      request('http://localhost:3000', function (err, res, html){
+        console.log(res.statusCode);
+        if(res.statusCode === 200){
+          console.log(res.body);
+          assert.equal(res.body,"Hello, world!");
+          done();
+        }
     });
   });
 });
