faa94e00cb2b843ca99dbf88e7a32bdd0194cd6b
stanley zheng <dev.stanzheng@gmail.com>
2014-09-01 23:43:29 -0400

    add persistent and a test driver

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server(3000);
 
+var db = require('mongoskin').db('localhost:27017/scorpion');
+
 server.route({
     method: 'GET',
-    path: '/',
+    path: '/job',
     handler: function (request, reply) {
-        reply('Hello, world!');
+        reply(db.scorpion.find());
     }
 });
 
 server.route({
     method: 'GET',
-    path: '/{name}',
+    path: '/job/{id}',
     handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+        reply('Job, ' + encodeURIComponent(request.params.id) + '!');
     }
 });
 
